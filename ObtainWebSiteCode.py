bba700b02435966bd0e864f9f2b9862a22afe233
jackfcarlson <jackfcarlson@gmail.com>
2018-03-23 19:01:13 -0500

    Update ObtainWebSiteCode.py

--- ObtainWebSiteCode.py
+++ ObtainWebSiteCode.py
@@ -1,8 +1,10 @@
 import bs4 as bs
 import urllib.request
 
-sauce = urllib.request.urlopen('website URL Here').read()
+rawData = urllib.request.urlopen('website URL Here').read()
 
-soup = bs.BeautifulSoup(sauce, 'lxml')
+filteredPage = bs.BeautifulSoup(rawData, 'lxml')
 
-print(soup)
+#print(rawData) would contain print(filteredPage) along with /t and /n and other "invisible" characters.
+#filteredPage displays/prints what you would see if you went to a website and clicked "View Page Source".
+print(filteredPage)
